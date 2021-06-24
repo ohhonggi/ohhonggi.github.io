5d53a66b1a5d1844cee978bf99651939a44a7df4
ohhondgi <5ghdrl@naver.com>
2021-06-24 14:56:00 +0900

    MGK plan

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "포스팅 테스트",
-        "excerpt":"    ","categories": ["Blog"],
+        "title": "모각코 plan",
+        "excerpt":"목표: full-stack developer가 되기 위한 공부 팀장: 박상현 / 팀원: 오홍기, 유건열, 김영현, 김남호 활동 기간: 2021.06.30(수) ~ 2021.08.19(월) 중 6회 이상 html, css 기초 다지기 사용언어: html, css 공부 방법: DB과제를 통해 경험한 html과 css를 기반으로 기초 다지기 공부 내용: html 문법, js 문법, oracle DB 연동 목표:간단한 웹페이지...","categories": ["Blog"],
         "tags": ["Blog","jekyll","github","git"],
         "url": "/blog/firstPosting/",
-        "teaser": null
+        "teaser": "/assets/images/background.jpg"
       }]
