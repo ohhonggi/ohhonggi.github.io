69aab4b2f8883cbed814ccb6724538eff6544f44
ohhondgi <5ghdrl@naver.com>
2021-07-01 23:29:01 +0900

    first week finish

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Blog","jekyll","github","git"],
         "url": "/blog/firstPosting/",
         "teaser": "/assets/images/background.jpg"
+      },{
+        "title": "1주차",
+        "excerpt":"                                                                                                 오늘의 계획 : 생활코딩을 통한 react 공부                                                       개발환경 구축                              컴포넌트 제작                              state                               이벤트                                                                                오늘의 회고                                                       mac m1에서 docker 연동의 호환성 문제 해결을 위해 2시간정도 소요.                                                            생활코딩 react의 component 강의를 통해 react component example 사용                                                                                       ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","github","git"],
+        "url": "/blog/react-%EA%B3%B5%EB%B6%80-%EB%B0%8F-docker-%EC%84%B8%ED%8C%85/",
+        "teaser": "/assets/images/background.jpg"
       }]
